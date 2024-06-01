3730b41223be033e8c15d6dd28f8e14f70ae2d08
yuta925 <yutarou041@gmail.com>
2024-06-01 18:25:09 +0900

    ファイル操作

--- mainte/test.php
+++ mainte/test.php
@@ -1,8 +1,26 @@
 <?php
-// パスワードを記録したファイルの場所
-echo __FILE__;
-// /Applications/MAMP/htdocs/php_test/mainte/test.php
 
-echo '<br>';
-// パスワード
-echo password_hash('password123', PASSWORD_BCRYPT);
+$contactFile = '.contact.dat';
+
+// ファイル丸ごと読み込み
+$fileContents =  file_get_contents($contactFile);
+
+// echo $fileContents;
+
+// ファイルに書き込み(上書き)
+// file_put_contents($contactFile, 'テストです');
+
+// $addText = 'テストです' . "\n";
+
+// ファイルに書き込み(追記)s
+// file_put_contents($contactFile, $addText, FILE_APPEND);
+
+// 配列 file ,区切る explode, foreach
+$allData = file($contactFile);
+
+foreach ($allData as $lineData) {
+    $lines = explode(',', $lineData);
+    echo $lines[0] . '<br>';
+    echo $lines[1] . '<br>';
+    echo $lines[2] . '<br>';
+}
